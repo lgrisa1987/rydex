4b223e8b3d9f194f3400b22f7fbdfc99d54f2a79
Leandro <leagrisalena@gmail.com>
2026-09-06 00:15:45 -0300

    inicio proyecto

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "node:url";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  },
+});
